d85b61bfc7c6318d7a34f368a2bb51f3ececad3e
Tejas <73245011+ZERstar@users.noreply.github.com>
2022-11-30 01:43:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
 
         <div className="max-w-xl mx-auto p-8">
           <h1 className="font-bold text-2xl lg:text-3xl text-white pb-8 text-center">
-            IP Address Tracker
+            IP Address Locator
           </h1>
 
           <form
